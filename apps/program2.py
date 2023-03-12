322dbc7bbc43690fb5899dc80e7555bcbdc96999
Muhamad Rizki Kadafi <rizkikadafi11@gmail.com>
2023-03-12 15:09:41 +0700

    menambahkan nama program

--- apps/program2.py
+++ apps/program2.py
@@ -4,7 +4,7 @@ def main():
     print("doing some task")
 
 title = "========== Program 2: Test program 2 ==========\n" # untuk di tampilkan sebagai judul
-name = "Nama program 2" # untuk di tampilkan di list menu
+name = "Reverse String" # untuk di tampilkan di list menu
 description = ("Deskripsi untuk aplikasi\n", False) # deskripsi program
 program2 = App(name=name, title=title, description=description, program=main)
 
